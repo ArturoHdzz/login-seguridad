79a9400977bd9d1e79a752c58233f7420d0fcbe9
ArturoHdzz <hernandez020610@gmail.com>
2025-03-25 10:15:17 -0600

    canbio atras

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\Auth\LoginController;
  * - web: Session, CSRF, and cookies
  * - prevent-back: Prevent back navigation for security reasons
  */
-Route::middleware(['web'])->group(function () {
+Route::middleware(['web', 'prevent-back'])->group(function () {
     /**
      * Root route - Redirects to login
      */
